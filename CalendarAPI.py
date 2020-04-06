3dfc0777deb041af977555baf0c1e20c2e2d3526
christensensl <60920424+christensensl@users.noreply.github.com>
2020-04-05 20:16:57 -0700

    Add files via upload

--- CalendarAPI.py
+++ CalendarAPI.py
@@ -2,14 +2,13 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 import datefinder
 
-flow = InstalledAppFlow.from_client_secrets_file(
-    'clientsecret.json',
-    scopes=['https://www.googleapis.com/auth/calendar'])
-
+# Authentication to use the CalendarAPI
+flow = InstalledAppFlow.from_client_secrets_file('clientsecret.json', scopes=['https://www.googleapis.com/auth/calendar'])
 flow.run_local_server()
 credentials = flow.credentials
 service = build("calendar", "v3", credentials=credentials)
 
+# Create a Google Calendar event with the specified arguments.
 def create_event(start_time_str, end_time_str, summary, description=None, location=None):
     start_matches = list(datefinder.find_dates(start_time_str))
     end_matches = list(datefinder.find_dates(end_time_str))
