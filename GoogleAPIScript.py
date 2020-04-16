67ba6bafcbb332ed5f3ee772ed6cf9074fef35c4
christensensl <60920424+christensensl@users.noreply.github.com>
2020-04-15 17:19:31 -0700

    Add files via upload
    Combined the code from 2 files into 1 to simplify everything.

--- GoogleAPIScript.py
+++ GoogleAPIScript.py
@@ -1,8 +1,6 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from googleapiclient.discovery import build
-from CalendarAPI import create_event
-from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 import datefinder
 
@@ -13,8 +11,6 @@ creds = ServiceAccountCredentials.from_json_keyfile_name('creds.json', scopes)
 # Read/Parse information in the Google Sheet
 client = gspread.authorize(creds)
 sheet = client.open('shifts-by-client').sheet1
-service = build("calendar", "v3", credentials=creds)
-
 data = sheet.get_all_records()
 
 flow = InstalledAppFlow.from_client_secrets_file('clientsecret.json', scopes=['https://www.googleapis.com/auth/calendar'])
@@ -22,9 +18,31 @@ flow.run_local_server()
 credentials = flow.credentials
 service = build("calendar", "v3", credentials=credentials)
 
+def create_event(start_time_str, end_time_str, summary, description=None, location=None):
+    start_matches = list(datefinder.find_dates(start_time_str))
+    end_matches = list(datefinder.find_dates(end_time_str))
+    if len(start_matches):
+        start_time = start_matches[0]
+    if len(end_matches):
+        end_time = end_matches[0]
+
+    event = {
+        'summary': summary,
+        'location': location,
+        'description': description,
+        'start': {
+            'dateTime': start_time.strftime("%Y-%m-%dT%H:%M:%S"),
+            'timeZone': 'America/Los_Angeles',
+        },
+        'end': {
+            'dateTime': end_time.strftime("%Y-%m-%dT%H:%M:%S"),
+            'timeZone': 'America/Los_Angeles',
+        },
+    }
+    return service.events().insert(calendarId='primary', body=event).execute()
+
 # Define variables and iterate through each item. Then create calendar events on Google Calendar.
 # Data is a list of dictionaries with each dictionary containing shift info. This iterates through each dictionary and creates a calendar event.
-# create_event function is being pulled from a separate file.
 for dict in data:
     client_name = dict['Client']
     city = dict['Client City']
